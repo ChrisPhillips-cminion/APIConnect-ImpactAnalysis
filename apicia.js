bbd89a3bee37c6f592a0092d116d5f97fa42f014
BuildTools <chris.phillips@uk.ibm.com>
2017-12-19 13:33:54 +0000

    Fixed hte issue with json hood

--- apicia.js
+++ apicia.js
@@ -1,1 +1,20 @@
-"use strict";var f=require("./lib/functions.js"),config=require("./lib/config.js"),getData=require("./lib/getData.js");if(config.showHelp)f.showHelp();else{var handleUserInput=require("./lib/handleUserInput.js"),processDir=require("./lib/processDir.js");handleUserInput(config,function(){getData(config,function(e){processDir(e,config.displaySchema,function(e){if(console.error("\nResults of analysis: "),f.printDivideLine(),"json"===config.output)console.log(JSON.stringify(e));else{var i=require("json-hood");i.printJSONasArrowDiagram(e)}})})})}
+"use strict";
+var f = require("./lib/functions.js"),
+    config = require("./lib/config.js"),
+    getData = require("./lib/getData.js");
+if (config.showHelp) f.showHelp();
+else {
+    var handleUserInput = require("./lib/handleUserInput.js"),
+        processDir = require("./lib/processDir.js");
+    handleUserInput(config, function() {
+        getData(config, function(e) {
+            processDir(e, config.displaySchema, function(e) {
+                if (console.error("\nResults of analysis: "), f.printDivideLine(), "json" === config.output) console.log(JSON.stringify(e));
+                else {
+                    var i = require("json-hood");
+                    i.printJSONasArrowDiagram(e)
+                }
+            })
+        })
+    })
+}
