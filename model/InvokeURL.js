c04f075bf767bdef45fa4368a59b701cf60164d8
BuildTools <chris.phillips@uk.ibm.com>
2018-01-09 19:05:59 +0000

    Added support for policies

--- model/InvokeURL.js
+++ model/InvokeURL.js
@@ -1,1 +1,10 @@
-function InvokeURL(r){if("url"==typeof r)this.url=r;else{if("string"!=typeof r)throw new Error("Url Must be a URL object or a string not "+typeof r);this.url=urlImport.parse(r)}this.type="http"}var urlImport=require("url");module.exports=InvokeURL;
+function InvokeURL(r) {
+    if ("url" == typeof r) this.url = r;
+    else {
+        if ("string" != typeof r) throw new Error("Url Must be a URL object or a string not " + typeof r);
+        this.url = urlImport.parse(r)
+    }
+    this.type = "http"
+}
+var urlImport = require("url");
+module.exports = InvokeURL;
---
+++ model/Policy.js
@@ -0,0 +1,13 @@
+// Copyright IBM Corp. 2017. All Rights Reserved.
+// Licensed under "The MIT License (MIT)"
+/*
+      Licensed Materials - Property of IBM
+      © IBM Corp. 2016
+*/
+
+function Policy(name, version) {
+	this.name=name;
+	this.version = version;
+	this.type = "policy";
+}
+module.exports = Policy;
