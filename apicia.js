ea93490e5806461e43de0dd5d039547815f1d67b
BuildTools <chris.phillips@uk.ibm.com>
2018-02-05 23:26:53 +0000

    Added support for Secuirty and Implemented policies in a better way

--- apicia.js
+++ apicia.js
@@ -4,17 +4,45 @@ var f = require("./lib/functions.js"),
     getData = require("./lib/getData.js");
 if (config.showHelp) f.showHelp();
 else {
-    var handleUserInput = require("./lib/handleUserInput.js"),
-        processDir = require("./lib/processDir.js");
+    var handleUserInput = require("./lib/handleUserInput.js")
+
     handleUserInput(config, function() {
         getData(config, function(e) {
-            processDir(e, config, function(e) {
-                if (console.error("\nResults of analysis: "), f.printDivideLine(), "json" === config.output) console.log(JSON.stringify(e));
-                else {
-                    var i = require("json-hood");
-                    i.printJSONasArrowDiagram(e)
-                }
-            })
+            f.printDivideLine()
+
+            if (config.dataType.indexOf("Invokes and Proxies") > -1) {
+var processDir = undefined;
+                processDir = require("./lib/processDir.js");
+                processDir(e, config, function(e) {
+                    if (console.error("Results of Invoke/Proxy analysis: "), f.printDivideLine(), "json" === config.output) console.log(JSON.stringify(e));
+                    else {
+                        var i = require("json-hood");
+                        i.printJSONasArrowDiagram(e)
+                    }
+                })
+            }
+            if (config.dataType.indexOf("Security") > -1) {
+var processDir = undefined;
+                processDir = require("./lib/processSecurity.js");
+                processDir(e, config, function(e) {
+                    if (console.error("Results of Security analysis: "), f.printDivideLine(), "json" === config.output) console.log(JSON.stringify(e));
+                    else {
+                        var i = require("json-hood");
+                        i.printJSONasArrowDiagram(e)
+                    }
+                })
+            }
+            if (config.dataType.indexOf("Policies") > -1) {
+var processDir = undefined;
+                processDir = require("./lib/processDirPolicy.js");
+                processDir(e, config, function(e) {
+                    if (console.error("Results of Policy analysis: "), f.printDivideLine(), "json" === config.output) console.log(JSON.stringify(e));
+                    else {
+                        var i = require("json-hood");
+                        i.printJSONasArrowDiagram(e)
+                    }
+                })
+            }
         })
     })
 }
