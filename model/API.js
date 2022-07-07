fe4d7302e3d661fffe6d84d2241a268b30c2d4d9
ChrisPhillips-cminion <chris.phillips@uk.ibm.com>
2022-07-07 14:16:13 +0100

    added basepath to the tool output

--- model/API.js
+++ model/API.js
@@ -1,10 +1,11 @@
-function API(i,t,s){
+function API(i,t,s,basepath){
   this.invokes=[],
   this.version=t,
   this.name=i,
   this.title=s,
   this.security={},
   this.policies=[]
+  this.basepath=basepath
 }
 
 module.exports=API;
